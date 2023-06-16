74303aaae227323ec9c0b3f1d028202d781e68f6
Frederico Proença <joopereight@gmail.com>
2023-06-16 20:50:20 -0300

    Commented.

--- Assets/Scripts/Flock.cs
+++ Assets/Scripts/Flock.cs
@@ -4,90 +4,90 @@ using UnityEngine;
 
 public class Flock : MonoBehaviour
 {
-    public FlockManager flockManager;
-    float flockSpeed;
-    bool isTurning = false;
+    public FlockManager flockManager; // Referência para o FlockManager que gerencia o cardume
+    float flockSpeed; // Velocidade atual do cardume
+    bool isTurning = false; // Indica se o cardume está virando
     void Start()
     {
-        flockSpeed = Random.Range(flockManager.minSpeed, flockManager.maxSpeed);
+        flockSpeed = Random.Range(flockManager.minSpeed, flockManager.maxSpeed); // Define a velocidade inicial do cardume dentro do intervalo especificado no FlockManager
     }
     void Update()
     {
-        Bounds bounds = new Bounds(flockManager.transform.position, flockManager.swimLimits * 2);
+        Bounds bounds = new Bounds(flockManager.transform.position, flockManager.swimLimits * 2); // Define os limites dentro dos quais o cardume pode nadar
         RaycastHit hit;
-        Vector3 flockDirection = flockManager.transform.position - transform.position;
+        Vector3 flockDirection = flockManager.transform.position - transform.position; // Direção para a qual o cardume deve se mover
         if (!bounds.Contains(transform.position))
         {
-            isTurning = true;
-            flockDirection = flockManager.transform.position - transform.position;
+            isTurning = true; // Se o cardume estiver fora dos limites, ele precisa virar
+            flockDirection = flockManager.transform.position - transform.position; // Atualiza a direção para a posição do FlockManager
         }
         else if (Physics.Raycast(transform.position, transform.forward * 50, out hit))
         {
-            isTurning = true;
-            flockDirection = Vector3.Reflect(transform.forward, hit.normal);
+            isTurning = true; // Se houver um obstáculo à frente, o cardume precisa virar
+            flockDirection = Vector3.Reflect(transform.forward, hit.normal); // Reflete a direção do cardume com base na normal do obstáculo
         }
         else
         {
-            isTurning = false;
+            isTurning = false; // O cardume não está virando
         }
         if (isTurning)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation,
                 Quaternion.LookRotation(flockDirection),
-                flockManager.rotationSpeed * Time.deltaTime);
+                flockManager.rotationSpeed * Time.deltaTime); // Faz o cardume virar gradualmente na direção desejada
         }
         else
         {
             if (Random.Range(0, 100) < 10)
             {
-                flockSpeed = Random.Range(flockManager.minSpeed, flockManager.maxSpeed);
+                flockSpeed = Random.Range(flockManager.minSpeed, flockManager.maxSpeed); // Altera aleatoriamente a velocidade do cardume com uma probabilidade de 10%
             }
             if (Random.Range(0, 100) < 20)
             {
-                ApplyFlockRules();
+                ApplyFlockRules(); // Aplica as regras de comportamento do cardume com uma probabilidade de 20%
             }
         }
-        transform.Translate(0, 0, Time.deltaTime * flockSpeed);
+        transform.Translate(0, 0, Time.deltaTime * flockSpeed); // Move o cardume na direção atual com base na velocidade atual
     }
-
     void ApplyFlockRules()
     {
         GameObject[] allFlocks;
-        allFlocks = flockManager.allFlock;
-        Vector3 flockCenter = Vector3.zero;
-        Vector3 flockAvoidance = Vector3.zero;
-        float flockAvgSpeed = 0.01f;
-        float neighborDistance;
-        int flockSize = 0;
-
+        allFlocks = flockManager.allFlock; // Obtém todos os objetos do cardume
+        Vector3 flockCenter = Vector3.zero; // Centro do cardume
+        Vector3 flockAvoidance = Vector3.zero; // Direção de evitação do cardume
+        float flockAvgSpeed = 0.01f; // Velocidade média do cardume
+        float neighborDistance; // Distância entre os objetos do cardume
+        int flockSize = 0; // Quantidade de objetos no cardume
         foreach (GameObject flock in allFlocks)
         {
             if (flock != this.gameObject)
             {
                 neighborDistance = Vector3.Distance(flock.transform.position, transform.position);
 
-                if (neighborDistance <= flockManager.neighborDistance)
+                if (neighborDistance <= flockManager.neighborDistance) // Verifica se o objeto do cardume está dentro da distância de vizinhança
                 {
                     flockCenter += flock.transform.position;
                     flockSize++;
 
                     if (neighborDistance < 1f)
                     {
-                        flockAvoidance += (transform.position - flock.transform.position);
+                        flockAvoidance += (transform.position - flock.transform.position); // Calcula a direção de evitação caso os objetos estejam muito próximos
                     }
+
                     Flock anotherFlock = flock.GetComponent<Flock>();
-                    flockAvgSpeed += anotherFlock.flockSpeed;
+                    flockAvgSpeed += anotherFlock.flockSpeed; // Calcula a velocidade média do cardume
                 }
             }
-        }
-        if (flockSize > 0)
-        {
-            flockCenter /= flockSize;
-            flockSpeed = flockAvgSpeed / flockSize;
-            Vector3 direction = (flockCenter - flockAvoidance) - transform.position;
-            if (direction != Vector3.zero)
+            if (flockSize > 0)
             {
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), flockManager.rotationSpeed * Time.deltaTime);
+                flockCenter /= flockSize; // Calcula o centro do cardume dividindo a soma das posições dos objetos pela quantidade de objetos
+                flockSpeed = flockAvgSpeed / flockSize; // Define a velocidade do cardume como a velocidade média dos objetos
+                Vector3 direction = (flockCenter - flockAvoidance) - transform.position; // Calcula a direção para a qual o cardume deve se mover
+
+                if (direction != Vector3.zero)
+                {
+                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), flockManager.rotationSpeed * Time.deltaTime); // Faz o cardume virar gradualmente na direção desejada
+                }
             }
         }
     }
--- Assets/Scripts/FlockManager.cs
+++ Assets/Scripts/FlockManager.cs
@@ -4,44 +4,46 @@ using UnityEngine;
 
 public class FlockManager : MonoBehaviour
 {
-    public GameObject birdPrefab;
-    public int numBirds = 20;
-    public GameObject[] allFlock;
-    public Vector3 swimLimits = new Vector3(5, 5, 5);
-    public Vector3 goalPosition;
-    public GameObject goal;
+    public GameObject flockPrefab; // Prefab do cardume
+    public int numFlocks = 20; // Número de cardumes
+    public GameObject[] allFlock; // Array que armazena todos os objetos do cardume
+    public Vector3 swimLimits = new Vector3(5, 5, 5); // Limites dentro dos quais o cardume pode nadar
+    public Vector3 goalPosition; // Posição do objetivo do cardume
+    public GameObject goal; // Objeto do objetivo
 
     [Header("Bird Configuration")]
     [Range(0f, 5f)]
-    public float minSpeed;
+    public float minSpeed; // Velocidade mínima dos cardumes
     [Range(0f, 5f)]
-    public float maxSpeed;
+    public float maxSpeed; // Velocidade máxima dos cardumes
     [Range(1f, 10f)]
-    public float neighborDistance;
+    public float neighborDistance; // Distância entre os cardumes para determinar a vizinhança
     [Range(5f, 5f)]
-    public float rotationSpeed;
+    public float rotationSpeed; // Velocidade de rotação dos cardumes
 
     void Start()
     {
-        allFlock = new GameObject[numBirds];
-        for (int i = 0; i < numBirds; i++)
+        allFlock = new GameObject[numFlocks]; // Inicializa o array com o tamanho definido pelo número de cardumes
+        for (int i = 0; i < numFlocks; i++)
         {
             Vector3 position = transform.position + new Vector3(
-                Random.Range(-swimLimits.x, swimLimits.x),
+                Random.Range(-swimLimits.x, swimLimits.x), // Gera uma posição aleatória dentro dos limites especificados
                 Random.Range(-swimLimits.y, swimLimits.y),
                 Random.Range(-swimLimits.z, swimLimits.z));
-            allFlock[i] = Instantiate(birdPrefab, position, Quaternion.identity);
-            allFlock[i].GetComponent<Flock>().flockManager = this;
+            allFlock[i] = Instantiate(flockPrefab, position, Quaternion.identity); // Instancia o cardume no cenário na posição gerada
+            allFlock[i].GetComponent<Flock>().flockManager = this; // Define o FlockManager deste cardume como o FlockManager atual
         }
-        goalPosition = transform.position;
+        goalPosition = transform.position; // Define a posição do objetivo como a posição atual do FlockManager
     }
+
     void Update()
     {
-        goalPosition = transform.position;
-        if (Random.Range(0, 100) < 10)
+        goalPosition = transform.position; // Atualiza a posição do objetivo como a posição atual do FlockManager
+
+        if (Random.Range(0, 100) < 10) // Com uma probabilidade de 10%, muda a posição do objetivo
         {
             goalPosition = transform.position + new Vector3(
-                Random.Range(-swimLimits.x, swimLimits.x),
+                Random.Range(-swimLimits.x, swimLimits.x), // Gera uma nova posição aleatória dentro dos limites especificados
                 Random.Range(-swimLimits.y, swimLimits.y),
                 Random.Range(-swimLimits.z, swimLimits.z));
         }
